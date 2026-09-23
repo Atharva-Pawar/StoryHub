ef792af8fed30995ddd7ac35e8a4fce87079ef9f
Atharva-Pawar <atharvapawar846@gmail.com>
2026-09-24 00:30:02 +0530

    create blog page

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,14 +1,21 @@
-import { Avatar } from './BlogCard'
+import { Link } from "react-router-dom";
+import { Avatar } from "./BlogCard";
 
 const Appbar = () => {
   return (
     <div>
-      <div className='border-b flex justify-between p-4'>
-        <div className='flex flex-col justify-center'>Medium</div>
-        <div className='flex flex-col justify-center'><Avatar name='Atharva'/></div>
+      <div className="border-b flex justify-between p-4">
+        <Link to={`/blogs`}>
+          <div className="flex flex-col justify-center cursor-pointer">
+            Medium
+          </div>
+        </Link>
+        <div className="flex flex-col justify-center">
+          <Avatar name="Atharva" />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Appbar
+export default Appbar;
--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,7 +1,23 @@
+import { useParams } from "react-router-dom"
+import { useBlog } from "../hooks"
+import BlogPage from "../components/BlogPage"
+
 const Blog = () => {
+
+  const {id} = useParams()
+  const {loading, blog} = useBlog({
+    id: id || ""
+  })
+
+  if(loading){
+    return <div>
+      loading...
+    </div>
+  }
+
   return (
     <>
-      blog
+      <BlogPage blog={blog} />
     </>
   )
 }
