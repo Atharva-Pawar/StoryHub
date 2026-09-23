f41ecb9d84fdd88df8d959ad817fc489f590d63d
Atharva-Pawar <atharvapawar846@gmail.com>
2026-09-23 18:00:29 +0530

    create blogs pages

---
+++ frontend/src/components/BlogCard.tsx
@@ -0,0 +1,45 @@
+interface BlogCaedProps {
+  authorName: string;
+  title: string;
+  content: string;
+  publishedDate: string;
+}
+const BlogCard = ({
+  authorName,
+  title,
+  content,
+  publishedDate,
+}: BlogCaedProps) => {
+  return (
+    <>
+      <div className="border-b border-slate-300 pb-3.5 p-4">
+        <div className="flex">
+          <div className="flex justify-center flex-col">
+            <Avatar name={authorName} />
+          </div>
+          <div className="font-extralight pl-2 flex justify-center flex-col">
+            {authorName}
+          </div>
+          <div className="pl-2 font-thin text-slate-500 flex justify-center flex-col">
+            {publishedDate}
+          </div>
+        </div>
+        <div className="text-xl font-semibold">{title}</div>
+        <div className="text-md font-thin">{content.slice(0, 100) + "..."}</div>
+        <div className="text-sm font-thin text-slate-400">{`${Math.ceil(content.length / 100)} minutes`}</div>
+      </div>
+    </>
+  );
+};
+
+function Avatar({ name }: { name: string }) {
+  return (
+    <>
+      <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-400 rounded-full">
+        <span className="font-medium text-body">{name[0]}</span>
+      </div>
+    </>
+  );
+}
+
+export default BlogCard;
---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,36 @@
+import BlogCard from "../components/BlogCard";
+
+const Blogs = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="max-w-xl">
+        <BlogCard
+          authorName="Atharva Pawar"
+          title="Titel of the book this is the big title for the book and change it"
+          content="content of the book Titel of the book this is the big title for the book and change it Titel of the book this is the big title for the book and change it"
+          publishedDate="2nd feb 2026"
+        />
+        <BlogCard
+          authorName="Atharva Pawar"
+          title="Titel of the book this is the big title for the book and change it"
+          content="content of the book Titel of the book this is the big title for the book and change it Titel of the book this is the big title for the book and change it"
+          publishedDate="2nd feb 2026"
+        />
+        <BlogCard
+          authorName="Atharva Pawar"
+          title="Titel of the book this is the big title for the book and change it"
+          content="content of the book Titel of the book this is the big title for the book and change it Titel of the book this is the big title for the book and change it"
+          publishedDate="2nd feb 2026"
+        />
+        <BlogCard
+          authorName="Atharva Pawar"
+          title="Titel of the book this is the big title for the book and change it"
+          content="content of the book Titel of the book this is the big title for the book and change it Titel of the book this is the big title for the book and change it"
+          publishedDate="2nd feb 2026"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Blogs;
