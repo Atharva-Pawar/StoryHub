c5da3ae345e85ed285206f7adb347d34384cb49e
Atharva-Pawar <atharvapawar846@gmail.com>
2026-09-26 19:15:03 +0530

    add loading for blogs

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const Loading = () => {
   return (
-    <div>
-      
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-8 h-8 border-4 border-slate-300 border-t-slate-700 rounded-full animate-spin"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
